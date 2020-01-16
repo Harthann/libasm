dfcb641f65ae796f357e6256157535e664b8f5b7
Harthann <nca.eyraud@gmail.com>
2020-01-16 11:45:04 +0100

    adding commentary

--- ft_strdup.s
+++ ft_strdup.s
@@ -10,18 +10,18 @@ _ft_strdup:
 	call _ft_strlen
 	push rdi; push sur la stack rdi (la chaine de caractere)
 	lea rdi, [rax + 1] ; place dans rdi le resultat de strlen + 1
-	call _malloc
-	xor rcx, rcx
-	pop rdi
+	call _malloc ; appel malloc en lui passant rdi en parametre
+	xor rcx, rcx ; initialise rcx a 0 qui sers de compteur a la loop
+	pop rdi ; recupere la chaine de caractere de la stack dans rdi
 	jmp .loop
 
-.loop:
-	cmp byte [rdi + rcx], 0
-	jz .ret
-	mov dl, [rdi + rcx]
-	mov [rax + rcx], dl
-	inc rcx
-	jmp .loop
+.loop:							; boucle qui copie srcs dans dest
+	cmp byte [rdi + rcx], 0		; check le \0
+	jz .ret						; si cmp a renvoyer 0 alors on es arrive au bout
+	mov dl, [rdi + rcx]			; place dans dl le caractere dans la source
+	mov [rax + rcx], dl			; lace dans dest le cracte dans dl
+	inc rcx						; incremente rcx
+	jmp .loop					; recommence la boucle
 
 .ret:
 	mov byte [rax + rcx], 0
