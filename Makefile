edd185053cd68156ca5ff60f9f97dad925ffcf4e
Harthann <nca.eyraud@gmail.com>
2020-01-23 10:25:03 +0100

    organisation

--- Makefile
+++ Makefile
@@ -1,18 +1,22 @@
 NAME = libasm.a
 
-SRCS = ft_strlen.s \
+SRC_FILE = ft_strlen.s \
 		ft_write.s \
 		ft_strcpy.s \
 		ft_strcmp.s \
 		ft_read.s \
 		ft_strdup.s
 
+SRC_PATH = srcs/
+
 OBJS = $(SRCS:%.s=%.o)
+SRCS = $(addprefix $(SRC_PATH), $(SRC_FILE))
 
 all : $(NAME)
 
 $(NAME) : $(OBJS) $(MAIN)
-	@ar rc libasm.a $(OBJS)
+	@ar rc $(NAME) $(OBJS)
+	@echo "$(NAME) Created"
 
 %.o : %.s
 	@nasm -f macho64 $< -o $@
--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -8,11 +8,11 @@ section .text
 	global _ft_strdup
 _ft_strdup:
 	call _ft_strlen
-	push rdi; push sur la stack rdi (la chaine de caractere)
-	lea rdi, [rax + 1] ; place dans rdi le resultat de strlen + 1
-	call _malloc ; appel malloc en lui passant rdi en parametre
-	xor rcx, rcx ; initialise rcx a 0 qui sers de compteur a la loop
-	pop rdi ; recupere la chaine de caractere de la stack dans rdi
+	push rdi			; push sur la stack rdi (la chaine de caractere)
+	lea rdi, [rax + 1]	; place dans rdi le resultat de strlen + 1
+	call _malloc		; appel malloc en lui passant rdi en parametre
+	xor rcx, rcx		; initialise rcx a 0 qui sers de compteur a la loop
+	pop rdi				; recupere la chaine de caractere de la stack dans rdi
 	jmp .loop
 
 .loop:							; boucle qui copie srcs dans dest
